ffe0e13f67abdd27610fc82d338460a853da974b
Grazielli Lima Berti <70613625+bertiGrazi@users.noreply.github.com>
2021-12-19 14:45:33 -0300

    delete storyboard and fix appDelegate

---
+++ gamePlay/gamePlay/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  gamePlay
+//
+//  Created by Grazielli Berti on 19/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
